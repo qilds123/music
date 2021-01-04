a1457212347f847e449ffc0a42d5d5b097dae0eb
qilds123 <makegash159789123@gmail.com>
2021-01-04 20:30:50 +0800

    Add files via upload

---
+++ scripts/playlist.js
@@ -0,0 +1,19 @@
+trackList = [
+  {
+    name:"War Drums of the Far East",
+    artist:"TeknoAxe",
+    src:"music/War_Drums_of_the_Far_East.mp3"
+  },
+          
+  {
+    name: "Chiptune Does Dubstep",
+    artist:"TeknoAxe",
+    src: "music/Chiptune_Does_Dubstep.mp3"
+  },
+          
+  {
+    name: "Angels Flying Through Hell",
+    artist:"TeknoAxe",
+    src: "music/Angels_Flying_Through_Hell.mp3"
+  }
+];
